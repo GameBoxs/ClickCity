dccae9068b47e36c8270ecb5d7dd4d52288c20cb
GameBoxs <ji_hun23@naver.com>
2021-07-19 21:39:32 +0900

    07/19commit
    1. 로그아웃 스크립트 작성
    2. 유저 정보 불러오는 스크립트작성

---
+++ Script/ReadUserInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BackEnd;
+public class ReadUserInfo : MonoBehaviour
+{
+    void InitalizeUserInfo()
+    {
+        Backend.BMember.GetUserInfo((callback) =>
+        {
+            string nickname = callback.GetReturnValuetoJSON()["row"]["gamerId"].ToString();
+            Debug.Log(nickname);
+        });
+    }
+    void Start()
+    {
+        InitalizeUserInfo();
+    }
+    public void OnclickLogout()
+    {
+        Backend.BMember.Logout();
+        Debug.Log("로그아웃 되었음");
+        InitalizeUserInfo();
+    }
+}
