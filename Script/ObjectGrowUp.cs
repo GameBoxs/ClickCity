5715bb486cdedc2fcd7322ddc9e08819d4543659
GameBoxs <ji_hun23@naver.com>
2021-07-29 23:35:36 +0900

    07-29commit
    1. One 레벨 디자인
    2. 오브젝트 팝업효과 스크립트

---
+++ Script/ObjectGrowUp.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectGrowUp : MonoBehaviour
+{
+    public float delay=0.05f;
+    List<Transform> childs;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        childs = new List<Transform>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            childs.Add(transform.GetChild(i));
+            //transform.GetChild(i).gameObject.transform.localScale.x = 0f;
+            //transform.GetChild(i).gameObject.transform.localScale.Set(0f, 1f, 1f);
+            transform.GetChild(i).gameObject.transform.localScale = new Vector3(0, 0, 0);
+        }
+
+        StartCoroutine(Enable());
+    }
+
+    IEnumerator Enable()
+    {
+        int i = 0, iterations = Mathf.RoundToInt(delay / Time.deltaTime) * 200;
+        float max = 1.6f, average = 1f, x = 0f;
+        if (iterations == 0)
+            iterations = 1;
+        while (childs.Count > 0)
+        {
+            yield return new WaitForSeconds(delay);
+            for (int a = 0; a < iterations; a++)
+            {
+                if (childs.Count == 0)
+                    yield break;
+                i = Random.Range(0, childs.Count);
+                //childs[i].gameObject.SetActive(true);
+                while (x <= max)
+                {
+                    childs[i].gameObject.transform.localScale = new Vector3(x, x, x);
+                    x += 0.1f;
+                    yield return new WaitForSeconds(0.01f);
+                }
+                while(x >= average)
+                {
+                    childs[i].gameObject.transform.localScale = new Vector3(x, x, x);
+                    x -= 0.1f;
+                    yield return new WaitForSeconds(0.01f);
+                }
+                childs.RemoveAt(i);
+                Random.InitState(a);
+            }
+        }
+    }
+}
